542cafd781ed33ac11635a3ff3766dbf45e837b5
tek1229 <tek1229@hotmail.com>
2018-06-25 20:18:31 -0400

    Add files via upload

--- fridge_from_freezer.ino
+++ fridge_from_freezer.ino
@@ -1,14 +1,15 @@
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
 //added code to turn a freezer on and off at 38 degrees to turn it into a fridge temporarily..
-#include "DHT.h"
+//added code by Thomas Dupras 6/25/2018
 
+#include "DHT.h"
 #define DHTPIN 2     // what pin we're connected to
-#define RelayPin 4 // pin connected to SSR  High to turn on, other side of SSR to gnd
-#define SetTemp 38 // set temperature for SSR control
+#define RelayPin 13 // pin connected to SSR  High to turn on, other side of SSR to gnd
+#define SetTemp 38 // set temperature for SSR control  same default temp as my fridge
 // Uncomment whatever type you're using!
-#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // Connect pin 1 (on the left) of the sensor to +5V
@@ -25,7 +26,7 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);   // left serial in for debugging.. removed at completetion of project.. stands alone
   Serial.println("DHTxx test!");
   pinMode(RelayPin, OUTPUT); 
 
@@ -35,8 +36,8 @@ float tempF = 0 ;
 
 void loop() {
   // Wait a few seconds between measurements.
-  delay(2000);   // Will have to change this to something like 30 seconds or so?? Maybe a minute
- // Read temperature as Fahrenheit (isFahrenheit = true)
+ 
+ // Read temperature as Fahrenheit (is Fahrenheit = true)
    tempF = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again).
@@ -50,6 +51,9 @@ void loop() {
   Serial.print(tempF);
   Serial.println(" *F\t");
  if ( tempF > SetTemp)  digitalWrite(RelayPin,HIGH);  // if too warm then turn on compressor
- else if (tempF < (SetTemp-2)) digitalWrite(RelayPin,LOW);  // if temp is 2 degrees colder than setpoint turn off compressor
- 
+ else if (tempF < (SetTemp-2)) {
+  digitalWrite(RelayPin,LOW);  // if temp is 2 degrees colder than setpoint turn off compressor
+  delay(300000);  // big 5 minute delay to help prevent slugging compressor from turning back on to rapidly..
+ }
+  delay(6000);   // Just slowing the whole loop down a little.. helped with serial debugging.. 
 }
